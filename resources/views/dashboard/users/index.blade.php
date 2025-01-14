291dc83c79b228f47be4b628f87f3d74b162c0a7
fathysamir <01154857080fa@gmail.com>
2025-01-14 14:20:30 +0200

    Initial commit

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -42,8 +42,7 @@
   .popup-content {
       margin: auto;
       display: block;
-      width: 80%;
-      height: 100%;
+     
       border-radius: 8px;
       box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
   }
