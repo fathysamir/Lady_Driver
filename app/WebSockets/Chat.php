3902247a1362ca422eaf1059f4b2a52429d9b606
fathysamir <01154857080fa@gmail.com>
2025-01-22 02:51:49 +0200

    Initial commit

--- app/WebSockets/Chat.php
+++ app/WebSockets/Chat.php
@@ -208,7 +208,7 @@ class Chat implements MessageComponentInterface {
        
         $eligibleDriverIds = [];
        
-
+dd($eligibleCars);
         foreach ($eligibleCars as $car) {
             $eligibleDriverIds[] = $car->user_id;
             if($car->owner->device_token){
