a5ceee1832f2f5abbb84dbe6176627d00f0d48b3
fathysamir <01154857080fa@gmail.com>
2025-03-03 22:11:15 +0200

    fathy

--- app/Http/Controllers/API/DriverController.php
+++ app/Http/Controllers/API/DriverController.php
@@ -373,8 +373,12 @@ class DriverController extends ApiController
         if (!$driving_license) {
             return $this->sendError(null, "You don't add information of your driving license yet", 400);
         }
+        $user = auth()->user();
         $driving_license->license_front_image = getFirstMediaUrl($driving_license, $driving_license->LicenseFrontImageCollection);
         $driving_license->license_back_image = getFirstMediaUrl($driving_license, $driving_license->LicenseBackImageCollection);
+        $driving_license->ID_front_image = getFirstMediaUrl($user, $user->IDfrontImageCollection);
+        $driving_license->ID_back_image = getFirstMediaUrl($user, $user->IDbackImageCollection);
+
         return $this->sendResponse($driving_license, null, 200);
     }
 
