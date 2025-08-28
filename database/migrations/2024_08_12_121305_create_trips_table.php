1ba4b90bfae7e5f665c469b0eb9d2fd49f996c07
Fathy Samir <01154857080fa@gmail.com.com>
2025-08-28 23:38:34 +0300

    auth controller

--- database/migrations/2024_08_12_121305_create_trips_table.php
+++ database/migrations/2024_08_12_121305_create_trips_table.php
@@ -46,7 +46,7 @@ return new class extends Migration
             $table->unsignedBigInteger('trip_cancelling_reason_id')->nullable();
             $table->foreign('trip_cancelling_reason_id')->references('id')->on('trip_cancelling_reasons')->onDelete('cascade');
             $table->enum('payment_status', ['unpaid', 'partial', 'paid','refunded'])->default('unpaid');
-            $table->enum('payment_method', ['cash', 'online'])->default('cash');
+            $table->enum('payment_method', ['cash', 'wallet'])->default('cash');
             $table->enum('student_trip', ['1', '0'])->default('0');
             $table->enum('type', ['car', 'comfort_car','scooter'])->default('car');
             $table->float('driver_stare_rate', 3, 2)->default(0);
