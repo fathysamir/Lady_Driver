5937e6d89e7f0d0e4dfd08e93c0433e9b7558ef0
Fathy Samir <01154857080fa@gmail.com.com>
2025-09-08 22:47:53 +0300

    auth controller

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -303,7 +303,7 @@ class AuthController extends ApiController
             'payment_method' => $method,
             'status'         => 'PENDING',
         ]);
-       
+       dd($payload);
         // call correct method
         if ($method === 'PayAtFawry') {
             $resp = $this->fawry->createReferenceCharge($payload);
