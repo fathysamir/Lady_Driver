8a98e941295b598528fa307ab0b4be3d41aa1ebd
Fathy Samir <01154857080fa@gmail.com.com>
2025-09-09 00:35:32 +0300

    auth controller

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -172,7 +172,7 @@ class AuthController extends ApiController
         }
 
         $v = Validator::make($request->all(), [
-            'paymentMethod'         => 'required|string|in:PayAtFawry,PayUsingCC,MWALLET',
+            'paymentMethod'         => 'required|string|in:PayAtFawry,PayUsingCC,MWallet',
             'amount'                => 'required|numeric|min:0.01',
             'customerMobile'        => 'required|string',
             'customerEmail'         => 'required|email',
@@ -185,10 +185,10 @@ class AuthController extends ApiController
             'cardExpiryYear'        => 'required_if:paymentMethod,PayUsingCC|nullable|string',
             'cardExpiryMonth'       => 'required_if:paymentMethod,PayUsingCC|nullable|string',
             'cvv'                   => 'required_if:paymentMethod,PayUsingCC|nullable|string',
-            'returnUrl'             => 'required_if:paymentMethod,PayUsingCC,MWALLET|nullable|url',
+            'returnUrl'             => 'required_if:paymentMethod,PayUsingCC,MWallet|nullable|url',
 
-            'walletMobile'          => 'required_if:paymentMethod,MWALLET|nullable|string',
-            'walletProviderService' => 'required_if:paymentMethod,MWALLET|nullable|string',
+            'walletMobile'          => 'required_if:paymentMethod,MWallet|nullable|string',
+            'walletProviderService' => 'required_if:paymentMethod,MWallet|nullable|string',
         ]);
 
         // if ($v->fails()) {
@@ -233,7 +233,7 @@ class AuthController extends ApiController
                 );
                 break;
 
-            case 'MWALLET':
+            case 'MWallet':
                 $sig = $this->fawry->makeWalletSignature(
                     $merchantRefNum,
                     strval(auth()->user()->id),
@@ -312,7 +312,7 @@ class AuthController extends ApiController
         }
 
         // extra fields for Wallet
-        if ($method === 'MWALLET') {
+        if ($method === 'MWallet') {
             $payload = [
                 'merchantCode'          => config('services.fawry.merchant_code'),
                 'merchantRefNum'        => $merchantRefNum,
