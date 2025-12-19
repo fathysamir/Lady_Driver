a293cd435a9fd32a2e3d9a1f38042b1024e8a0e8
Fathy Samir <01154857080fa@gmail.com.com>
2025-12-19 16:53:40 +0200

    mmm

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,9 @@ use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\ClientController;
 use App\Http\Controllers\API\DriverController;
 use App\Http\Controllers\API\LiveLocationController;
+use App\Models\Car;
+use App\Models\DriverLicense;
+use App\Models\Scooter;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,9 +22,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/test-test', function () {
-    $users = User::where('mode', 'client')->where('status', 'pending')->where('is_verified', '0')->get();
+    $users = User::where('mode', 'driver')->where('status', 'pending')->where('is_verified', '0')->get();
     foreach ($users as $user) {
         deleteMedia($user, $user->avatarCollection);
+        deleteMedia($user, $user->IDfrontImageCollection);
+        deleteMedia($user, $user->IDbackImageCollection);
+        deleteMedia($user, $user->passportImageCollection);
+        $lis = DriverLicense::where('user_id', $user->id)->first();
+        if ($lis) {
+            deleteMedia($lis, $lis->LicenseFrontImageCollection);
+            deleteMedia($lis, $lis->LicenseBackImageCollection);
+            $lis->delete();
+        }
+        if ($user->car) {
+            $car = Car::where('user_id', $user->id)->first();
+            deleteMedia($car, $car->avatarCollection);
+            deleteMedia($car, $car->PlateImageCollection);
+            deleteMedia($car, $car->LicenseFrontImageCollection);
+            deleteMedia($car, $car->LicenseBackImageCollection);
+            deleteMedia($car, $car->CarInspectionImageCollection);
+            $car->delete();
+        } elseif ($user->scooter) {
+            $scooter = Scooter::where('user_id', $user->id)->first();
+            deleteMedia($lis, $lis->avatarCollection);
+            deleteMedia($scooter, $scooter->PlateImageCollection);
+            deleteMedia($scooter, $scooter->LicenseFrontImageCollection);
+            deleteMedia($scooter, $scooter->LicenseBackImageCollection);
+            $scooter->delete();
+        }
+
         $user->delete();
     }
     return true;
