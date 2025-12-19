cce8d31c6b1d45c6adb1670b7506022abc3ecd63
Fathy Samir <01154857080fa@gmail.com.com>
2025-12-19 17:05:45 +0200

    mmm

--- app/Jobs/DeleteUnverifiedUsers.php
+++ app/Jobs/DeleteUnverifiedUsers.php
@@ -34,9 +34,28 @@ class DeleteUnverifiedUsers implements ShouldQueue
             ->where('created_at', '<=', Carbon::now()->subHour())
             ->first();
         Student::where('user_id', $user->id)->delete();
-        DriverLicense::where('user_id', $user->id)->delete();
-        Car::where('user_id', $user->id)->delete();
-        Scooter::where('user_id', $user->id)->delete();
+        $lis = DriverLicense::where('user_id', $user->id)->first();
+        if ($lis) {
+            deleteMedia($lis, $lis->LicenseFrontImageCollection);
+            deleteMedia($lis, $lis->LicenseBackImageCollection);
+            $lis->delete();
+        }
+        if ($user->car) {
+            $car = Car::where('user_id', $user->id)->first();
+            deleteMedia($car, $car->avatarCollection);
+            deleteMedia($car, $car->PlateImageCollection);
+            deleteMedia($car, $car->LicenseFrontImageCollection);
+            deleteMedia($car, $car->LicenseBackImageCollection);
+            deleteMedia($car, $car->CarInspectionImageCollection);
+            $car->delete();
+        } elseif ($user->scooter) {
+            $scooter = Scooter::where('user_id', $user->id)->first();
+            deleteMedia($lis, $lis->avatarCollection);
+            deleteMedia($scooter, $scooter->PlateImageCollection);
+            deleteMedia($scooter, $scooter->LicenseFrontImageCollection);
+            deleteMedia($scooter, $scooter->LicenseBackImageCollection);
+            $scooter->delete();
+        }
         $user->delete();
     }
 }
