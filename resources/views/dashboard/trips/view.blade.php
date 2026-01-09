0018b1944b62d9df6d4e277e2da2242ea2de4ebb
Fathy Samir <01154857080fa@gmail.com.com>
2026-01-10 00:11:33 +0200

    mmm

--- resources/views/dashboard/trips/view.blade.php
+++ resources/views/dashboard/trips/view.blade.php
@@ -1,6 +1,7 @@
 @extends('dashboard.layout.app')
 @section('title', 'Dashboard - view trip')
 @section('content')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <style>
         .star-rating {
             color: #FFf;
