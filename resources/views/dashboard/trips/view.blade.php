8180525308531fb85ba632c36970ce35a0654f6c
Fathy Samir <01154857080fa@gmail.com.com>
2026-01-17 23:31:56 +0200

    mmm

--- resources/views/dashboard/trips/view.blade.php
+++ resources/views/dashboard/trips/view.blade.php
@@ -397,10 +397,10 @@
             routeRenderers = [];
 
             if (tripStatus === "in_progress") {
-                drawRoute(startLocation, vehicleLocation, '#99FF80', waypoints);
+                drawRoute(startLocation, vehicleLocation, '#99c680', waypoints);
                 drawRoute(vehicleLocation, finalLocation, '#0000FF', waypoints);
             } else if (tripStatus === "completed") {
-                drawRoute(startLocation, finalLocation, '#99FF00', waypoints);
+                drawRoute(startLocation, finalLocation, '#99c680', waypoints);
             } else if (tripStatus === "pending") {
                 drawRoute(startLocation, finalLocation, '#0000FF', waypoints);
             } else if (tripStatus === "cancelled") {
