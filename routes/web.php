4033f8e476ed76929de934d2a60873b4e200402e
Mahmoud <mahmoudsaid.ifekra@gmail.com>
2026-02-18 16:01:49 +0200

    reset

--- routes/web.php
+++ routes/web.php
@@ -59,10 +59,7 @@ Route::get('/open-reset', function (Request $request) {
     $email = $request->query('email');
 
     //App and web links
-   // $appLink ="myapp://reset-password?token={$token}&email={$email}";
-   $appLink = "Ladydrive://reset-password?token=" . urlencode($token)
-          . "&email=" . urlencode($email);
-
+    $appLink ="myapp://reset-password?token={$token}&email={$email}";
     $webLink = url("/reset-password/{$token}?email={$email}");
 
     return view('emails.open-reset', compact('appLink', 'webLink'));
