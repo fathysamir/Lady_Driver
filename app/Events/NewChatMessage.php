d154caca58595dd5b2e224d52b2c23e677e0fd70
Mahmoud <mahmoudsaid.ifekra@gmail.com>
2026-03-12 22:57:06 +0200

    fix image profile

--- app/Events/NewChatMessage.php
+++ app/Events/NewChatMessage.php
@@ -38,7 +38,7 @@ class NewChatMessage implements ShouldBroadcast
             'trip_id'  => $this->chat->trip_id,
             'sender'   => $this->chat->sender_id,
             'sender_name' => $this->chat->sender->name,
-            'sender_image' => $this->chat->sender->avatar,
+            'sender_image' => $this->chat->sender->image,
             'message'  => $this->chat->message,
             'location' => $this->chat->location,
             'image'    => $this->chat->image,
