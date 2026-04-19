9fff2722fd20490bf06b75692babca1797582655
Mahmoud <mahmoudsaid.ifekra@gmail.com>
2026-04-19 22:20:13 +0200

    back

--- app/Events/TrackCar.php
+++ app/Events/TrackCar.php
@@ -12,7 +12,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
 
-class TrackCar implements ShouldBroadcast
+class TrackCar implements ShouldBroadcast, ShouldQueue
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
@@ -20,32 +20,16 @@ class TrackCar implements ShouldBroadcast
     public $lng;
     public $heading;
     public $speed;
-    public $distanceToPickup;
-    public $durationToPickup;
-    public $distanceToDestination;
-    public $durationToDestination;
     public $receiverId;
 
-    public function __construct(
-        $lat,
-        $lng,
-        $heading,
-        $speed,
-        $distanceToPickup,
-        $durationToPickup,
-        $distanceToDestination,
-        $durationToDestination,
-        $receiverId
-    ) {
-        $this->lat                   = $lat;
-        $this->lng                   = $lng;
-        $this->heading               = $heading;
-        $this->speed                 = $speed;
-        $this->distanceToPickup      = $distanceToPickup;
-        $this->durationToPickup      = $durationToPickup;
-        $this->distanceToDestination = $distanceToDestination;
-        $this->durationToDestination = $durationToDestination;
-        $this->receiverId            = $receiverId;
+    public function __construct($lat, $lng, $heading, $speed, $receiverId)
+    {
+
+    $this->lat        = floatval($lat);
+    $this->lng        = floatval($lng);
+    $this->heading    = $heading !== null ? floatval($heading) : 0;
+    $this->speed      = $speed !== null ? floatval($speed) : 0;
+    $this->receiverId = $receiverId;
     }
 
     public function broadcastOn()
@@ -61,17 +45,11 @@ class TrackCar implements ShouldBroadcast
     public function broadcastWith()
     {
         return [
-            'type' => 'track_car',
-            'data' => [
-                'lat'                     => $this->lat,
-                'lng'                     => $this->lng,
-                'heading'                 => $this->heading,
-                'speed'                   => $this->speed,
-                'distance_to_pickup'      => $this->distanceToPickup,
-                'duration_to_pickup'      => $this->durationToPickup,
-                'distance_to_destination' => $this->distanceToDestination,
-                'duration_to_destination' => $this->durationToDestination,
-            ],
+                'lat'     => $this->lat,
+                'lng'     => $this->lng,
+                'heading' => $this->heading,
+                'speed'   => $this->speed,
         ];
+
     }
 }
