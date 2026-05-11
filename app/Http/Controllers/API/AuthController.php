b419c58152db5a8e4a52d869b7133f6c54dcf013
Mahmoud <mahmoudsaid.ifekra@gmail.com>
2026-05-11 18:01:46 +0300

    trips count profile

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -1123,27 +1123,30 @@ class AuthController extends ApiController
     }
 
     public function profile($id)
-    {
-        $user                = User::where('id', $id)->with('city:id,name,name_ar')->first();
-        $user->image         = getFirstMediaUrl($user, $user->avatarCollection);
-        $user->ID_frontImage = getFirstMediaUrl($user, $user->IDfrontImageCollection);
-        $user->ID_backImage  = getFirstMediaUrl($user, $user->IDbackImageCollection);
-        $user->PassportImage = getFirstMediaUrl($user, $user->passportImageCollection);
-        $user->medical_examination_image = getFirstMediaUrl($user, $user->medicalExaminationImageCollection);
-        $user->criminal_record_image = getFirstMediaUrl($user, $user->criminalRecordImageCollection);
+{
+    $user                = User::where('id', $id)->with('city:id,name,name_ar')->first();
+    $user->image         = getFirstMediaUrl($user, $user->avatarCollection);
+    $user->ID_frontImage = getFirstMediaUrl($user, $user->IDfrontImageCollection);
+    $user->ID_backImage  = getFirstMediaUrl($user, $user->IDbackImageCollection);
+    $user->PassportImage = getFirstMediaUrl($user, $user->passportImageCollection);
+    $user->medical_examination_image = getFirstMediaUrl($user, $user->medicalExaminationImageCollection);
+    $user->criminal_record_image     = getFirstMediaUrl($user, $user->criminalRecordImageCollection);
 
+    if ($user->mode == 'client') {
+        $clientTrips        = Trip::where('user_id', $user->id)->where('status', 'completed');
+        $user->rate         = $clientTrips->clone()->where('driver_stare_rate', '>', 0)->avg('driver_stare_rate') ?? 0.00;
+        $user->trips_count  = $clientTrips->count();
 
-
-        if ($user->mode == 'client') {
-            $user->rate = Trip::where('user_id', $user->id)->where('status', 'completed')->where('driver_stare_rate', '>', 0)->avg('driver_stare_rate') ?? 0.00;
-        } elseif ($user->mode == 'driver') {
-            $user->rate = Trip::whereHas('car', function ($query) use ($user) {
-                $query->where('user_id', $user->id);
-            })->where('status', 'completed')->where('client_stare_rate', '>', 0)->avg('client_stare_rate') ?? 0.00;
-        }
-        return $this->sendResponse($user, null, 200);
+    } elseif ($user->mode == 'driver') {
+        $driverTrips        = Trip::whereHas('car', function ($query) use ($user) {
+                                $query->where('user_id', $user->id);
+                              })->where('status', 'completed');
+        $user->rate         = $driverTrips->clone()->where('client_stare_rate', '>', 0)->avg('client_stare_rate') ?? 0.00;
+        $user->trips_count  = $driverTrips->count();
     }
 
+    return $this->sendResponse($user, null, 200);
+}
     public function edit_personal_info(Request $request)
     {
         $rules = [
