bde6e632bd5dec056a9ca55f31f57fdf35f7604a
Mahmoud <mahmoudsaid.ifekra@gmail.com>
2026-05-12 15:44:17 +0300

    activation

--- app/Http/Controllers/API/DriverController.php
+++ app/Http/Controllers/API/DriverController.php
@@ -919,6 +919,21 @@ if ($trip->scooter) {
 
         return $this->sendResponse($trips->values(), null, 200);
     }
+    public function activation()
+    {
+        $user = auth()->user();
+        if ($user->is_online == '1') {
+            $user->is_online = '0';
+            $user->save();
+            return $this->sendResponse(null, 'you are Offline', 200);
+        } else {
+            $user->is_online = '1';
+            $user->save();
+            return $this->sendResponse(null, 'you are online', 200);
+
+        }
+
+    }
     public function driver_current_trip()
     {
         $check_account = $this->check_banned();
