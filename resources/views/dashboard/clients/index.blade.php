785b505f762881b3430866f46ae74c77c984c00b
Mahmoud <mahmoudsaid.ifekra@gmail.com>
2026-05-23 23:46:14 +0300

    mm

--- resources/views/dashboard/clients/index.blade.php
+++ resources/views/dashboard/clients/index.blade.php
@@ -83,7 +83,7 @@
     .export-dropdown-menu a {
         display: block;
         padding: 8px 16px;
-        background-color: #1a3c2e;
+        background-color: #000000;
         color: #212529;
         text-decoration: none;
         font-size: 0.875rem;
@@ -97,7 +97,7 @@
     }
 
     .export-dropdown-menu a:hover {
-        background-color: #14502e;
+        background-color: #1a3c2e ;
         color: #212529;
         text-decoration: none;
     }
