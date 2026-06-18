ab89c700ee68a6f38b1472c2546390c62150b348
Mahmoud <mahmoudsaid.ifekra@gmail.com>
2026-06-18 18:56:22 +0300

    revert restore

--- resources/views/dashboard/drivers/index_archives.blade.php
+++ resources/views/dashboard/drivers/index_archives.blade.php
@@ -51,12 +51,6 @@
                 <div class="col-lg-12">
                     <div class="card">
                         <div class="card-body">
-                            @if(session('error'))
-    <div class="alert alert-danger">{{ session('error') }}</div>
-@endif
-@if(session('success'))
-    <div class="alert alert-success">{{ session('success') }}</div>
-@endif
                             <div>
                                 <form id="searchForm" class="search-bar"
                                     style="margin-bottom:1%;margin-right:20px;margin-left:0px;" method="post"
